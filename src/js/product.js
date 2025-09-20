5f70ead8f00834ac44e67170719ef34b3e9fd65c
BrazSAP <codebrazsap@gmail.com>
2025-09-19 21:05:18 -0400

    Create product

--- src/js/product.js
+++ src/js/product.js
@@ -1,8 +1,12 @@
 import { getLocalStorage, setLocalStorage, getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
+import ProductDetails from "./ProductDetails.mjs";
 
-const dataSource = new ProductData("tents");
-const productId = getParam("product");
+export const dataSource = new ProductData("tents");
+export const productId = getParam("product");
+
+const product = new ProductDetails(productId, dataSource);
+product.init();
 
 console.log(dataSource.findProductById(productId));
 
